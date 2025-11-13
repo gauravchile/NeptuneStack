4bc9f46df00ce7be132bd111ab936da8dc0285cf
Gaurav Chile <gauravchile@users.noreply.github.com>
2025-11-13 18:32:42 +0000

    Dockerhub Script Update

--- dockerhub-username.sh
+++ dockerhub-username.sh
@@ -3,25 +3,21 @@
 NAME="$1"
 
 # Pattern without quotes
-OLD_WEB="<docker-username>/neptune-web"
-OLD_API="<docker-username>/neptune-api"
+OLD="<docker-username>"
 
-NEW_WEB="${NAME}/neptune-web"
-NEW_API="${NAME}/neptune-api"
+NEW="${NAME}"
 
 # Dev overlay
-sed -i "s#${OLD_WEB}#${NEW_WEB}#g" k8s/overlays/dev/kustomization.yaml
-sed -i "s#${OLD_API}#${NEW_API}#g" k8s/overlays/dev/kustomization.yaml
+sed -i "s#${OLD}#${NEW}#g" k8s/overlays/dev/kustomization.yaml
 
 # Prod overlay
-sed -i "s#${OLD_WEB}#${NEW_WEB}#g" k8s/overlays/prod/kustomization.yaml
-sed -i "s#${OLD_API}#${NEW_API}#g" k8s/overlays/prod/kustomization.yaml
+sed -i "s#${OLD}#${NEW}#g" k8s/overlays/prod/kustomization.yaml
 
 # Base frontend
-sed -i "s#${OLD_WEB}#${NEW_WEB}#g" k8s/base/frontend/deployment.yaml
-sed -i "s#${OLD_API}#${NEW_API}#g" k8s/base/frontend/deployment.yaml
+sed -i "s#${OLD}#${NEW}#g" k8s/base/frontend/deployment.yaml
 
 # Base backend
-sed -i "s#${OLD_WEB}#${NEW_WEB}#g" k8s/base/backend/deployment.yaml
-sed -i "s#${OLD_API}#${NEW_API}#g" k8s/base/backend/deployment.yaml
+sed -i "s#${OLD}#${NEW}#g" k8s/base/backend/deployment.yaml
 
+# Makefile
+sed -i "s#${OLD}#${NEW}#g" Makefile
